a666bda5ff48ed2e03b0431201158ea4227cd236
Alexandro Cu <alexandro.cuma@gmail.com>
2025-10-22 15:43:27 -0600

    chore: remove unused toggle flag from CLI root command

--- cmd/root.go
+++ cmd/root.go
@@ -155,7 +155,6 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.Flags().BoolVarP(&skipConfirm, "yes", "y", false, "Skip confirmation and commit immediately")
 	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Show verbose output")
 }
